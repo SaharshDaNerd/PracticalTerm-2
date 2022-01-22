274bc789ccf6ac45aeff90a494eb572021627731
Saharsh Saxena <saharshcodes@gmail.com>
2022-01-22 14:30:55 +0530

    program10

--- Program10.py
+++ Program10.py
@@ -1,6 +1,12 @@
+#Create a dictionary that asks for roll number,marks and name of the student n number of times.
 n = int(input("Enter the number of students: "))
-dic = {}
-for i in range n:
-    roll=int(input("Enter the roll number: "))
-    name=int(input("Enter the name: "))
-    
+d = {}
+for i in range(n):
+    roll = int(input("Enter the roll number: "))
+    name = input("Enter the name: ")
+    marks = int(input("Enter the marks: "))
+    d[roll] = [name,marks]
+#Print the name of students who scored more than 75 marks.
+for i in d:
+    if d[i][1]>75:
+        print(d[i][0])
